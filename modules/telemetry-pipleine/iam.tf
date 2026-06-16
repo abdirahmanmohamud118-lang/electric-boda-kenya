3cdd41cd91250f0bf8c193a84b2157a9304d083b
abdirahmanmohamud118-lang <abdirahmanmohamud118@gmail.com>
2026-06-16 11:12:57 +0300

    first update

---
+++ modules/telemetry-pipleine/iam.tf
@@ -0,0 +1,192 @@
+#############################################
+# lambda execution role and policy
+#############################################
+resource "aws_iam_role" "lambda_role" {
+  name = "electric-boda-${var.environment}-lambda-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_policy" "lambda_policy" {
+  name        = "electric-boda-${var.environment}-lambda-policy"
+  description = "IAM policy for electric-boda telemetry lambda processing data in ${var.environment}"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "arn:aws:logs:*:*:*"
+      },
+
+      {
+        Effect = "Allow"
+        Action = [
+          "kinesis:DescribeStream",
+          "kinesis:DescribeStreamSummary",
+          "kinesis:GetRecords",
+          "kinesis:GetShardIterator",
+          "kinesis:ListShards"
+        ]
+        Resource = "*"
+      },
+
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:PutItem",
+          "dynamodb:GetItem",
+          "dynamodb:UpdateItem"
+        ]
+        Resource = "*"
+      },
+
+      {
+        Effect = "Allow"
+        Action = [
+          "sns:Publish"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+resource "aws_iam_role_policy_attachment" "lambda_role_policy_attachment" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
+}
+
+
+#############################################
+# iot core role and policy
+#############################################
+
+
+resource "aws_iam_role" "iot_role" {
+  name = "electric-boda-${var.environment}-iot-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "iot.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_policy" "iot_policy" {
+  name        = "electric-boda-${var.environment}-iot-policy"
+  description = "IAM policy for electric-boda telemetry iot core processing data in ${var.environment}"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "kinesis:PutRecord",
+          "kinesis:PutRecords"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "firehose:PutRecord",
+          "firehose:PutRecordBatch"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "iot_role_policy_attachment" {
+  role       = aws_iam_role.iot_role.name
+  policy_arn = aws_iam_policy.iot_policy.arn
+}
+
+
+
+
+#############################################
+# firehose role and policy
+#############################################
+
+resource "aws_iam_role" "firehose_role" {
+  name = "electric-boda-${var.environment}-firehose-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "firehose.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_policy" "firehose_policy" {
+  name        = "electric-boda-${var.environment}-firehose-policy"
+  description = "IAM policy for electric-boda telemetry firehose processing data in ${var.environment}"
+
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "kinesis:DescribeStream",
+          "kinesis:GetShardIterator",
+          "kinesis:GetRecords"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject",
+          "s3:PutObjectAcl"
+        ]
+        Resource = "arn:aws:s3:::${var.analytics_bucket_name}/*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetBucketLocation"
+        ]
+        Resource = "arn:aws:s3:::${var.analytics_bucket_name}"
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "firehose_role_policy_attachment" {
+  role       = aws_iam_role.firehose_role.name
+  policy_arn = aws_iam_policy.firehose_policy.arn
+}
---
+++ modules/telemetry-pipleine/variables.tf
@@ -0,0 +1,53 @@
+variable "environment" {
+  description = "The deployment environment (e.g., 'dev', 'prod')."
+  type        = string
+  default     = "dev"
+
+
+  validation {
+    condition     = contains(["dev", "prod"], var.environment)
+    error_message = "Environment must be one of 'dev' or 'prod'."
+  }
+}
+
+variable "project_name" {
+  description = "The name of the project."
+  type        = string
+  default     = "electric-boda-kenya"
+}
+
+
+variable "kinesis_shard_count" {
+  description = "The number of shards for the Kinesis stream."
+  type        = number
+  default     = 1
+}
+
+
+variable "kinesis_retention_period_hours" {
+  description = "The retention period for the Kinesis stream in hours."
+  type        = number
+  default     = 24
+}
+
+variable "kinesis_stream_name" {
+  description = "The name of the Kinesis stream."
+  type        = string
+}
+
+variable "analytics_bucket_name" {
+  description = "The name of the S3 bucket for long-term telemetry storage"
+  type        = string
+}
+
+variable "battery_threshold" {
+  description = "The battery level threshold for alerts (in percentage) for the boda "
+  type        = number
+  default     = 15
+}
+
+
+variable "alert_email" {
+  description = "The email address to send alerts to."
+  type        = string
+}
